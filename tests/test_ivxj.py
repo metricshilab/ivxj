08a84ae35e7f7c67dcb6e3a40af725799777db3e
PanJi-0 <163510072+PanJi-0@users.noreply.github.com>
2024-09-26 10:34:42 +0800

    test: add more tests

--- tests/test_ivxj.py
+++ tests/test_ivxj.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from ivxj.ivxj import ivxj
 
 def test_ivxj():
@@ -6,17 +7,34 @@ def test_ivxj():
     for h in range(1,4):
         for i in range(1,5):
             # get test data
-            y = np.loadtxt(f'tests/y_h{h}_i{i}.csv', delimiter=',')
-            dX = np.loadtxt(f'tests/dX_h{h}_i{i}.csv', delimiter=',')
-            rhoz = np.loadtxt(f'tests/rhoz_h{h}_i{i}.csv', delimiter=',')
+            y = np.loadtxt(f'tests/y_h{h}_i{i}.csv', dtype=np.float64, delimiter=',')
+            dX = np.loadtxt(f'tests/dX_h{h}_i{i}.csv', dtype=np.float64, delimiter=',')
+            rhoz = np.loadtxt(f'tests/rhoz_h{h}_i{i}.csv', dtype=np.float64, delimiter=',')
             Tlens = np.loadtxt(f'tests/Tlens_h{h}_i{i}.csv', delimiter=',').astype(int)
-            X = np.loadtxt(f'tests/X_h{h}_i{i}.csv', delimiter=',')
-            expected_result = np.loadtxt(f'tests/result_h{h}_i{i}.csv', delimiter=',')
+            X = np.loadtxt(f'tests/X_h{h}_i{i}.csv', dtype=np.float64, delimiter=',')
+            expected_result = np.loadtxt(f'tests/result_h{h}_i{i}.csv', dtype=np.float64, delimiter=',')
 
             r1, r2, r3, r4 = ivxj(y, dX, rhoz, Tlens)
             r5, r6, r7, r8 = ivxj(y, X, rhoz, Tlens)
 
-            actual_result = np.array([r1, r2, r3, r4, r5, r6, r7, r8])
+            actual_result = np.array([r1, r2, r3, r4, r5, r6, r7, r8], dtype= np.float64)
 
-            assert (actual_result == expected_result).all(), "ivxj cumpute incorrectly!"
+            gap = np.abs(actual_result - expected_result)
+            relative_gap = np.abs((actual_result - expected_result)/expected_result)
+
+            print(f"Result for h={h}, i={i}\n")
+            result = np.column_stack((actual_result, expected_result, gap, relative_gap))
+            df = pd.DataFrame(result, columns=["Actual", "Expected", "Error", "Relative Error"])
+            print(df)
+            max_value = df["Relative Error"].max()
+            mean_value = df["Relative Error"].mean()
+            print("\nMaximun value in Relative Error:", max_value)
+            print("Mean value in Relative Error:", mean_value)
+
+            assert (relative_gap < 0.4).all(), "relative_gap is too large!"
+            assert (gap < 1).all(), "gap is too large!"
+
+            ivxj_gap = np.array([gap[1],gap[5]])
+
+            assert (ivxj_gap < 0.005).all(), "ivxj_gap is too large!"
     
