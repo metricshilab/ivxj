dab10c47be04994455c7ccd0c36e3b3d01590b82
PanJi-0 <163510072+PanJi-0@users.noreply.github.com>
2024-10-02 14:28:03 +0800

    test: updated test_ivxj

--- tests/test_ivxj.py
+++ tests/test_ivxj.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from ivxj.ivxj import ivxj
+from ivxj.ivxj import raw_ivxj
 
 def test_ivxj():
     """Test ivxj function using the paper data"""
@@ -14,13 +14,13 @@ def test_ivxj():
             X = np.loadtxt(f'tests/X_h{h}_i{i}.csv', dtype=np.float64, delimiter=',')
             expected_result = np.loadtxt(f'tests/result_h{h}_i{i}.csv', dtype=np.float64, delimiter=',')
 
-            r1, r2, r3, r4 = ivxj(y, dX, rhoz, Tlens)
-            r5, r6, r7, r8 = ivxj(y, X, rhoz, Tlens)
+            r1, r2, r3, r4 = raw_ivxj(y, dX, rhoz, Tlens)
+            r5, r6, r7, r8 = raw_ivxj(y, X, rhoz, Tlens)
 
             actual_result = np.array([r1, r2, r3, r4, r5, r6, r7, r8], dtype= np.float64)
 
             gap = np.abs(actual_result - expected_result)
             relative_gap = np.abs((actual_result - expected_result)/expected_result)
 
-            assert (relative_gap < 0.4).all(), "relative_gap is too large!"
+            assert (relative_gap < 0.001).all(), "relative_gap is too large!"
     
